37fbaad3e72e45b536ae27a278d877657e1ea156
Carles <carmar85@gmail.com>
2024-11-27 17:49:43 +0100

    EditProject page, form, api, react-query requests

---
+++ src/components/projects/EditProjectForm.tsx
@@ -0,0 +1,79 @@
+import { useForm } from "react-hook-form";
+import { Project, ProjectFormData } from "../../types";
+import { Link, useNavigate } from "react-router-dom";
+import ProjectForm from "./ProjectForm";
+import { useMutation } from "@tanstack/react-query";
+import { updateProject } from "../../api/ProjectAPI";
+import { toast } from "react-toastify";
+
+type EditProjectFormProps = {
+  data: ProjectFormData;
+  projectId: Project["_id"];
+};
+
+export default function EditProjectForm({
+  data,
+  projectId,
+}: EditProjectFormProps) {
+  const navigate = useNavigate();
+  const initialValues: ProjectFormData = {
+    projectName: data.projectName,
+    clientName: data.clientName,
+    description: data.description,
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: initialValues });
+
+  const { mutate } = useMutation({
+    mutationFn: updateProject,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: () => {
+      toast.success("Project updated");
+      navigate("/");
+    },
+  });
+
+  const handleForm = (formData: ProjectFormData) => {
+    const data = {
+      formData,
+      projectId,
+    };
+    mutate(data);
+  };
+
+  return (
+    <>
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-5xl font-black">Create Project</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">
+          Fill out the next form to edit the project
+        </p>
+        <nav className="my-5">
+          <Link
+            to="/"
+            className="bg-red-400 hover:bg-red-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+          >
+            Back to projects
+          </Link>
+        </nav>
+        <form
+          className="mt-10 bg-white shadow-lg p-10 rounded-lg"
+          onSubmit={handleSubmit(handleForm)}
+          noValidate
+        >
+          <ProjectForm register={register} errors={errors} />
+          <input
+            type="submit"
+            value="Save"
+            className="bg-red-400 hover:bg-red-500 p-3 w-full text-white uppercase font-bold cursor-pointer transition-colors"
+          />
+        </form>
+      </div>
+    </>
+  );
+}
--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import DashboardPage from "./pages/DashboardPage";
 import CreateProjectPage from "./pages/projects/CreateProjectPage";
+import EditProjectPage from "./pages/projects/EditProjectPage";
 
 export default function Router() {
   return (
@@ -9,10 +10,10 @@ export default function Router() {
       <Routes>
         <Route element={<AppLayout />}>
           <Route path="/" element={<DashboardPage />} index />
+          <Route path="/projects/create" element={<CreateProjectPage />} />
           <Route
-            path="/projects/create"
-            element={<CreateProjectPage />}
-            index
+            path="/projects/:projectId/edit"
+            element={<EditProjectPage />}
           />
         </Route>
       </Routes>
